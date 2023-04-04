64a8cc7a34a6bced728cd8b99373d73945ecb202
Natasha Kuksa <natasha.kuksa.96@gmail.com>
2023-04-04 22:09:39 +0200

    add css

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 const URL_NAME = 'https://restcountries.com/v3.1/name/';
-const FIELDS = 'name,capital,population,flags,lanuages';
+const FIELDS = 'name,capital,population,flags,languages';
 
 export function fetchCountries(name) {
   return fetch(`${URL_NAME}${name}?fields=${FIELDS}`).then(response => {
